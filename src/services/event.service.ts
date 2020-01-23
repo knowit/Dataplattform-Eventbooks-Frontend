9b461d71e3111aa0c3d263dd354bd3a3a7bef995
Gjøvåg <christopher.gjovag@knowit.no>
2020-01-23 09:52:07 +0100

    Make services consistent

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -1,8 +1,8 @@
 import API from '@aws-amplify/api';
-import { EVENT_API, EVENT_ENDPOINT } from '@/constants/api.constants';
+import { EVENT_API, EVENT_ENDPOINT, INGEST_API, INGEST_APIKEY } from '@/constants/api.constants';
 import { deserialize } from 'class-transformer';
 import Event from '@/models/event.model';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 
 export const getEvents = async () => {
@@ -19,5 +19,47 @@ export const verifyEventCode = (eventCode: String) => {
 
   return axios.get(url, {
     baseURL: EVENT_ENDPOINT,
+  });
+};
+
+export const sendVote = (eventCode: string, vote: number) => {
+  const url = '/VoteType';
+  const storedVote = localStorage.getItem(eventCode);
+  const previousVote = storedVote !== null ? Number(storedVote) : undefined;
+
+  localStorage.setItem(eventCode, vote.toString());
+
+  const data = {
+    event_code: eventCode,
+    current_vote: vote,
+    previous_vote: previousVote
+  };
+
+  return axios.post(url, data, {
+    baseURL: INGEST_API,
+    headers: {
+      'x-api-key': INGEST_APIKEY
+    }
+  });
+};
+
+export const sendComment = (eventCode: string, comment: string): Promise<void | AxiosResponse<any>> => {
+  if (comment === '') {
+    // Do not sent empty comment
+    return Promise.resolve();
+  }
+
+  const url = '/VoteType';
+  const currentVote = Number(localStorage.getItem(eventCode));
+  const data = {
+    event_code: eventCode,
+    current_vote: currentVote,
+    comment
+  };
+  return axios.post(url, data, {
+    baseURL: INGEST_API,
+    headers: {
+      'x-api-key': INGEST_APIKEY
+    }
   });
 };
